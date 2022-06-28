3daef3b76b6516adf7ab68ad059844ca70b83c6c
hajaf <hajafatounjie@gmail.com>
2022-06-28 13:14:23 -0400

    chal

--- scraping.py
+++ scraping.py
@@ -1,8 +1,8 @@
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-import pandas as pd
 import datetime as dt
+import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_image": hem_image(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,46 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hem_image(browser):
+        # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    False
+
+
+    # In[18]:
+
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    # Parse the html with beautifulsoup
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+
+    # Get the links for each of the 4 hemispheres
+    hemi_links = hemi_soup.find_all('h3')
+    # hemi_links
+
+    # loop through each hemisphere link
+    for hemi in hemi_links:
+        # Navigate and click the link of the hemisphere
+        img_page = browser.find_by_text(hemi.text)
+        img_page.click()
+        html= browser.html
+        img_soup = soup(html, 'html.parser')
+        # Scrape the image link
+        img_url = 'https://astrogeology.usgs.gov/' + str(img_soup.find('img', class_='wide-image')['src'])
+        # Scrape the title
+        title = img_soup.find('h2', class_='title').text
+        # Define and append to the dictionary
+        
+        hemi_dict = {'img_url': img_url,'title': title}
+        hemisphere_image_urls.append(hemi_dict)
+        browser.back()
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
